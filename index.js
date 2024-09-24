ec8783c442d17becf69892849ea16251e895ecf6
asifalam515 <asibulalam515@gmail.com>
2024-09-24 15:32:57 +0600

    added

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
 
 // mongodb server
 // user name = asibulalam516
 // password = t6VoeVPcsriDUmq9
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
   "mongodb+srv://asibulalam516:t6VoeVPcsriDUmq9@cluster0.um15t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
@@ -28,7 +28,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("usersDB");
+    const userCollection = database.collection("users");
     // Send a ping to confirm a successful connection
+    // get request
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // delete request
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("please delete id from database", id);
+      //   it will query which one it will delete
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    // api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      console.log("user created", result);
+      res.send(result);
+      console.log("new user", user);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
